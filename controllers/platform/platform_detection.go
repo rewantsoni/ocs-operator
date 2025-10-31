3f37e7e3f9a52497707657bea7a5da74b1cd8797
Rewant Soni <resoni@redhat.com>
2025-10-31 13:51:53 +0530

    don't skip objstore on aws
    Signed-off-by: Rewant Soni <resoni@redhat.com>

--- controllers/platform/platform_detection.go
+++ controllers/platform/platform_detection.go
@@ -23,7 +23,6 @@ var (
 
 	// SkipObjectStorePlatforms is a list of all PlatformTypes where CephObjectStores will not be deployed.
 	SkipObjectStorePlatforms = []configv1.PlatformType{
-		configv1.AWSPlatformType,
 		configv1.GCPPlatformType,
 		configv1.AzurePlatformType,
 		configv1.IBMCloudPlatformType,
